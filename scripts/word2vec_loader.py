20ce4406244670315314cb7f1ed2570c4836561b
esmason <esmason66@gmail.com>
2017-04-27 18:38:39 -0700

    Update word2vec_loader.py

--- scripts/word2vec_loader.py
+++ scripts/word2vec_loader.py
@@ -5,7 +5,3 @@ def load_word_vectors():
 	print("loading word vectors")
 	word_vectors = KeyedVectors.load_word2vec_format('../bin/GoogleNews-vectors-negative300.bin', binary=True)
 	return word_vectors
-	
-	
-
-
