4bd260ca4845fb9ca9f243678b85df044f8847e2
Paul Cernek <ptcernek@gmail.com>
2017-04-29 00:20:04 -0700

    Add script to count out-of-vocab words wrt skipthoughts

---
+++ scripts/count_oov.py
@@ -0,0 +1,106 @@
+"""
+Determine words in quora train and test set that are OOV (out of vocabulary)
+with respect to the skipthoughts model
+"""
+
+import argparse
+import os
+
+from collections import defaultdict
+import pickle
+
+import data_utils as du
+
+import kiros.skipthoughts as st
+
+class FileWriterStdoutPrinter:
+
+    def __init__(self, file_path):
+        self.fd = open(file_path, 'w')
+
+    def emit(self, text):
+        print text
+        fd.write(text)
+
+    def emit_line(self, text):
+        print text
+        fd.write(text + '\n')
+
+def analyze_oov(word_dict, quora_data, output_dir, prefix):
+    oov_word_freqs = defaultdict(lambda : 0)
+    oov_counts_per_question = defaultdict(lambda : 0)
+    num_tokens = 0
+
+    for col_label in ['question1', 'question2']:
+        raw_text = st.preprocess(quora_data[col_label])
+        for i, q in enumerate(raw_text):
+            num_oovs_in_cur_q = 0
+            tokens = q.split()
+            num_tokens += len(tokens)
+            for w in tokens:
+                if not w in word_dict:
+                    # Count the occurrence of this OOV word
+                    oov_word_freqs[w] += 1
+                    # Count the occurrence of an OOV word for this question
+                    num_oovs_in_cur_q += 1
+
+                    # Signal that the word is OOV with our special signal, which we checked
+                    # beforehand is not in train.csv or test.csv
+                    orig_text = quora_data[col_label][i]
+                    quora_data.set_value(i, col_label, orig_text.replace(w, '%<'+w+'>%'))
+
+            # Increment the histogram count for questions with this number of OOVs
+            oov_counts_per_question[num_oovs_in_cur_q] += 1
+
+    # Write everything to disk
+    oov_word_fd = open(os.path.join(output_dir, prefix + '_oov_word_freqs.pkl'), 'w')
+    pickle.dump(oov_word_freqs, oov_word_fd)
+    oov_word_fd.close()
+
+    counts_per_q_fd = open(os.path.join(output_dir, prefix + '_oov_per_q.pkl'), 'w')
+    pickle.dump(oov_counts_per_question, counts_per_q_fd)
+    counts_per_q_fd.close()
+
+    edited_df_file = os.path.join(output_dir, prefix + '_oov_annot.csv')
+    du.write_csv(quora_data, edited_df_file)
+
+    # Some summary stats to humor the user
+    writer.emit_line("Num oov words={0} (out of {1}, or {2}%)".format(len(oov_word_freqs), num_tokens, len(oov_word_freqs)/float(num_tokens)))
+    writer.emit_line("Num of q's containing oov words={0} (out of {1}, or {2}%)".format(len(oov_counts_per_question), len(quora_data)*2, float(len(oov_counts_per_question))/len(quora_data)*2))
+
+def analyze_dict(word_dict, writer):
+    writer.emit_line('Word_dict contains {0} unique entries'.format(len(word_dict)))
+
+def main():
+    parser = argparse.ArgumentParser(description='Evaluate a model on the quora question-pair dataset.')
+    parser.add_argument('--quora-data-dir', required=True, help='path to the directory containing the quora data')
+    parser.add_argument('--st-model-dir', required=True, help='path to the directory containing the skipthoughts model')
+    parser.add_argument('--output-dir', default='.', help='path to the directory to write to')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    args = parser.parse_args()
+
+    output_file = os.path.join(args.output_dir, 'word_counts.txt')
+    writer = FileWriterStdoutPrinter(output_file)
+
+    print "Loading skipthoughts model..."
+    model = st.load_model(args.st_model_dir)
+    print "Initializing skipthoughts word dict..."
+    word_dict = st.init_word_dict(model)
+    print "Analyzing word dict..."
+    analyze_dict(word_dict, )
+
+    print "Loading training set..."
+    train = du.load_csv(os.path.join(args.quora_data_dir, 'train.csv'))
+    writer.emit_line("Analyzing word counts in train.csv...")
+    analyze_oov(word_dict, train, writer)
+
+    # Be sure to write data to disk for train before moving on to test, which is much bigger
+
+    print "Loading test set..."
+    test = du.load_csv(os.path.join(args.quora_data_dir, 'test.csv'))
+    writer.emit_line("Analyzing word counts in test.csv...")
+    analyze_oov(word_dict, test, writer)
+
+
+if __name__ == "__main__":
+    main()
